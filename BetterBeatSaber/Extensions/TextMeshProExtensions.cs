be0f1e29078cbf8a4cd97191b31e54eba3170714
Steven Krahforst <19632836+StevenKrahforst@users.noreply.github.com>
2024-05-05 03:48:36 +0200

    👌 Little Update

--- BetterBeatSaber/Extensions/TextMeshProExtensions.cs
+++ BetterBeatSaber/Extensions/TextMeshProExtensions.cs
@@ -45,41 +45,4 @@ public static class TextMeshProExtensions {
         text.font = enable ? BloomFont ?? BeatSaberUI.MainTextFont : BeatSaberUI.MainTextFont;
     }
     
-    public static TMP_FontAsset Clone(this TMP_FontAsset original) {
-
-#if PRE_130
-        return TMP_FontAsset.CreateFontAsset(original.sourceFontFile);
-#else
-        
-        if (original.atlasPopulationMode == AtlasPopulationMode.Dynamic && original.sourceFontFile != null)
-            return TMP_FontAsset.CreateFontAsset(original.sourceFontFile);
-      
-        var instance = ScriptableObject.CreateInstance<TMP_FontAsset>();
-        
-        instance.SetField("m_Version", original.version);
-        instance.SetField("m_FaceInfo", original.faceInfo);
-        
-        instance.atlasPopulationMode = original.atlasPopulationMode;
-        instance.atlas = original.atlas;
-        instance.atlasTextures = original.atlasTextures;
-        
-        instance.SetField("m_AtlasWidth", original.atlasWidth);
-        instance.SetField("m_AtlasHeight", original.atlasHeight);
-        instance.SetField("m_AtlasPadding", original.atlasPadding);
-        instance.SetField("m_AtlasRenderMode", original.atlasRenderMode);
-
-        #if !PRE_130
-        instance.isMultiAtlasTexturesEnabled = original.isMultiAtlasTexturesEnabled;
-        #endif
-        
-        instance.material = new Material(original.material);
-        
-        instance.ReadFontAssetDefinition();
-        
-        return instance;
-
-#endif
-        
-    }
-
 }
